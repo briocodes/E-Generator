d6920b73be5df94a70f9cfef3d946fd45baf4e5e
Briocodes <briocodes@gmail.com>
2020-10-31 22:16:24 +0100

    methods

--- src/emailapp/EmailApp.java
+++ src/emailapp/EmailApp.java
@@ -4,7 +4,7 @@ public class EmailApp {
     public static void main(String[] args) {
         Email email = new Email("James","Belema");
 
-        email.setAlternateEmail("android@google.com");
+        email.setAlternateEmail("androidteam@google.com");
         System.out.println("Your alternate email is: " + email.getAlternateEmail());
     }
 }
